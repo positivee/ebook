c7dec035b3c20da13f869bc415f7594161417c47
Aleksandra <olka130697@wp.pl>
2019-12-09 20:09:10 +0100

    naprawione kilkukrotne wyświetlanie tych samych książek

--- app/Model/BookstoreSearchOffer.php
+++ app/Model/BookstoreSearchOffer.php
@@ -5,6 +5,7 @@ namespace App\Model;
 
 //model dla księgarni do wyszukiwania dodanych przez nią ofert
 
+use App\Book;
 use App\Dto\Offer\OfferFetchInput;
 use App\Dto\Offer\OfferOutputFactory;
 use Illuminate\Support\Facades\DB;
@@ -21,15 +22,13 @@ class BookstoreSearchOffer
                 'books.description', 'books.author_name', 'books.author_surname',
                 'books.category_id','offers.bookstore_id', 'offers.book_id', 'offers.price',
                 'offers.date_from', 'offers.date_to', 'offers.link', 'books.isbn_number')
-            ->orderBy('offers.date_to', 'ASC');
-
-        if($fetchInput->getBookstore()) {
-            $allOffersSelect->where('offers.bookstore_id', '=' , $fetchInput->getBookstore()->id);
-        }
+            ->where('offers.bookstore_id', '=' , $fetchInput->getBookstore()->id)
+            ->orderBy('offers.date_to', 'ASC')
+            ->get();
 
         $offerOutputArray = [];
 
-        foreach ($allOffersSelect->get() as $offer) {
+        foreach ($allOffersSelect as $offer) {
 
             $offerOutputArray[] = OfferOutputFactory::createFromRow($offer);
         }
@@ -39,18 +38,18 @@ class BookstoreSearchOffer
 
 
     public function showAllOffer() : array {
-        //metoda do wyswietlania wszystkich ofert na stronie dla uzytkowników
+        //metoda do wyswietlania wszystkich ofert
 
         $allOffersSelect = DB::table('offers')
             ->join('bookstores', 'bookstores.id', '=', 'offers.bookstore_id')
             ->join('books', 'books.id', '=', 'offers.book_id')
-            ->select('books.id','books.title', 'books.year', 'books.print', 'books.picture',
+            ->select('books.title','books.id', 'books.year', 'books.print', 'books.picture',
                 'books.description', 'books.author_name', 'books.author_surname',
                 'books.category_id','offers.bookstore_id', 'offers.book_id', 'offers.price',
                 'offers.date_from', 'offers.date_to', 'offers.link', 'books.isbn_number')
-     
             ->orderBy('books.title', 'ASC')
-            ->get();
+            ->get()->unique('title', true);
+
 
 
         $offerOutputArray = [];
--- app/Model/UserSearchOffer.php
+++ app/Model/UserSearchOffer.php
@@ -15,17 +15,15 @@ class UserSearchOffer
     {
 
         $booksTable = DB::table('offers')
-                        ->join('bookstores', 'bookstores.id', '=', 'offers.bookstore_id')
-                        ->join('books', 'books.id', '=', 'offers.book_id')
-                        ->join('categories', 'categories.id', '=', 'books.category_id')
-                        ->select('books.title', 'books.year', 'books.print', 'books.picture',
-                            'books.description', 'books.author_name', 'books.author_surname',
-                            'books.category_id','categories.name','offers.bookstore_id', 'offers.book_id', 'offers.price',
-                            'offers.date_from', 'offers.date_to', 'offers.link', 'books.isbn_number')
-                        ->orderBy('books.title', 'ASC');
+            ->join('bookstores', 'bookstores.id', '=', 'offers.bookstore_id')
+            ->join('books', 'books.id', '=', 'offers.book_id')
+            ->select('books.title','books.id', 'books.year', 'books.print', 'books.picture',
+                'books.description', 'books.author_name', 'books.author_surname',
+                'books.category_id','offers.bookstore_id', 'offers.book_id', 'offers.price',
+                'offers.date_from', 'offers.date_to', 'offers.link', 'books.isbn_number')
+            ->orderBy('books.title', 'ASC');
 
-
-        if ($fetchInput->getTitle() !== '') {
+       if ($fetchInput->getTitle() !== '') {
             $booksTable->where('title', 'like', '%' . $fetchInput->getTitle() . '%');
         }
 
@@ -60,7 +58,7 @@ class UserSearchOffer
         //var_export($booksTable->toSql());
         $offerOutputArray = [];
 
-        foreach ($booksTable->get() as $offer) {
+        foreach ($booksTable->get()->unique('title') as $offer) {
 
             //var_export($offer);
             $offerOutputArray[] = OfferOutputFactory::createFromRow($offer);
